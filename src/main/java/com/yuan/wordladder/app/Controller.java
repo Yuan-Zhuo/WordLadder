25f6ea0d87a608a01063e494a3552ad68eccfd79
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2019-04-13 09:34:07 +0800

    =init security

--- src/main/java/com/yuan/wordladder/app/Controller.java
+++ src/main/java/com/yuan/wordladder/app/Controller.java
@@ -6,11 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @RestController
 @SpringBootApplication
 public class Controller {
 
     @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/word")
     public Path path(@RequestParam(value="word1", defaultValue="") String str1,
                      @RequestParam(value="word2", defaultValue="") String str2) {
         Path ladder = new Path(str1, str2);
