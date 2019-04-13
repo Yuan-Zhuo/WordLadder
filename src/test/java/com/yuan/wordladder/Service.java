aa2be70594f866921b5333f348ec8ee7ee8e1ef2
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2019-04-13 16:24:30 +0800

    Finish actuator.

--- src/test/java/com/yuan/wordladder/Service.java
+++ src/test/java/com/yuan/wordladder/Service.java
@@ -7,9 +7,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.yuan.wordladder.func.WordLadder;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class Service {
 
     @Before
