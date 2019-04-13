0e7b6d77752fc398a266fe25627f6ea9b53e3863
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2019-04-13 12:04:09 +0800

    =security finish

--- src/main/java/cc/mrbird/web/controller/BrowserSecurityController.java
+++ src/main/java/cc/mrbird/web/controller/BrowserSecurityController.java
@@ -32,7 +32,7 @@ public class BrowserSecurityController {
         if (savedRequest != null) {
             String targetUrl = savedRequest.getRedirectUrl();
             if (StringUtils.endsWithIgnoreCase(targetUrl, ".html"))
-                redirectStrategy.sendRedirect(request, response, "/login.html");
+                redirectStrategy.sendRedirect(request, response, "login.html");
         }
         return "访问的资源需要身份认证！";
     }
